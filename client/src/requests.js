b8887b63670535a77fc831f0ab40084d4586fb8c
lmalunin@gmail.com <lmalunin@gmail.com>
2019-08-16 18:35:31 +0300

    12

---
+++ client/src/requests.js
@@ -0,0 +1,57 @@
+const endpointURL = 'http://localhost:9000/graphql';
+
+
+export async function loadJob(id) {
+
+    const query = `
+        query JobQuery($id: ID!){
+                job(id: $id) {
+                id
+                title
+                description
+                company{
+                    id
+                    name
+                }
+            }
+        }`;
+
+    const { job } = await graphqlRequest(query, { id });
+    return job;
+}
+
+export async function loadJobs() {
+
+    const query = `
+    {
+        jobs{
+            id
+            titlez
+            description
+            company{
+                id
+                name
+            }
+        }
+    }`;
+
+    const { jobs } = await graphqlRequest(query);
+
+    return jobs;
+}
+
+async function graphqlRequest(query, variables = {}) {
+    const response = await fetch(endpointURL, {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ query, variables })
+    });
+
+    const responseBody = await response.json();
+    if (responseBody.errors) {
+        const message = responseBody.errors.map(error => error.message).join('\n');
+        throw new Error(message);
+    }
+
+    return responseBody.data;
+}
--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const express = require('express');
 const expressJwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
 const db = require('./db');
+const { ApolloServer, gql } = require('apollo-server-express');
+const fs = require('fs');
+
+const typeDefs = fs.readFileSync('./schema.graphql', { encoding: 'utf-8' });
+const resolvers = require('./resolvers');
+
 
 const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
@@ -15,14 +21,17 @@ app.use(cors(), bodyParser.json(), expressJwt({
 }));
 
 app.post('/login', (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   const user = db.users.list().find((user) => user.email === email);
   if (!(user && user.password === password)) {
     res.sendStatus(401);
     return;
   }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
+  const token = jwt.sign({ sub: user.id }, jwtSecret);
+  res.send({ token });
 });
 
-app.listen(port, () => console.info(`Server started on port ${port}`));
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+app.listen(port, () => console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`));
